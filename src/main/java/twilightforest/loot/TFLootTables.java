3a0d71f84ec038100f4d27a0339e883a160490d9
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-11-25 21:02:32 -0700

    first iteration of new lich tower loot tables

--- src/main/java/twilightforest/loot/TFLootTables.java
+++ src/main/java/twilightforest/loot/TFLootTables.java
@@ -44,6 +44,7 @@ public class TFLootTables {
 	public static final TFLootTables LABYRINTH_DEAD_END = new TFLootTables("labyrinth_dead_end");
 	public static final TFLootTables TOWER_ROOM = new TFLootTables("tower_room");
 	public static final TFLootTables TOWER_LIBRARY = new TFLootTables("tower_library");
+	public static final TFLootTables TOWER_POTION = new TFLootTables("tower_potion");
 	public static final TFLootTables BASEMENT = new TFLootTables("basement");
 	public static final TFLootTables HUT_JUNK = new TFLootTables("hut_junk");
 	public static final TFLootTables FOUNDATION_BASEMENT = new TFLootTables("foundation_basement");
