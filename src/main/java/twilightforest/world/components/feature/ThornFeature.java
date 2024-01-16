7d9de6404650354a741c5e111bfa3e42622f9e88
drullkus <drullkus@gmail.com>
2024-01-16 11:40:41 -0800

    Fix Thorns being chronically slow to generate

--- src/main/java/twilightforest/world/components/feature/ThornFeature.java
+++ src/main/java/twilightforest/world/components/feature/ThornFeature.java
@@ -15,7 +15,6 @@ import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import twilightforest.init.TFBlocks;
-import twilightforest.util.WorldUtil;
 import twilightforest.world.components.feature.config.ThornsConfig;
 
 public class ThornFeature extends Feature<ThornsConfig> {
@@ -45,7 +44,7 @@ public class ThornFeature extends Feature<ThornsConfig> {
 			BlockPos dPos = pos.relative(dir, i);
 
 			// Makes it avoid the troll clouds
-			if (!avoidGiantCloud || dPos.getY() - 64 <= WorldUtil.getBaseHeight(world, dPos.getX(), dPos.getZ(), Heightmap.Types.MOTION_BLOCKING_NO_LEAVES)) {
+            if (!avoidGiantCloud || (world.hasChunk(pos.getX() >> 4, pos.getZ() >> 4) && dPos.getY() - 64 <= world.getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, dPos.getX(), dPos.getZ()))) {
 				if (Math.abs(dPos.getX() - oPos.getX()) < config.maxSpread() && Math.abs(dPos.getZ() - oPos.getZ()) < config.maxSpread() && canPlaceThorns(world, dPos)) {
 					world.setBlock(dPos, TFBlocks.BROWN_THORNS.get().defaultBlockState().setValue(RotatedPillarBlock.AXIS, dir.getAxis()), 1 | 2);
 					world.getChunk(dPos).markPosForPostprocessing(dPos);
