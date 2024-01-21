550a93941b77b5476dfe46dc31d94dd5d322965f
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-01-21 11:50:06 -0700

    fixed transformation powder transforming tamed mobs, moved all logic to 1 place

--- src/main/java/twilightforest/dispenser/TransformationDispenseBehavior.java
+++ src/main/java/twilightforest/dispenser/TransformationDispenseBehavior.java
@@ -15,6 +15,7 @@ import net.neoforged.neoforge.event.EventHooks;
 import twilightforest.TwilightForestMod;
 import twilightforest.init.TFRecipes;
 import twilightforest.init.TFSounds;
+import twilightforest.item.TransformPowderItem;
 
 import java.util.UUID;
 
@@ -25,41 +26,12 @@ public class TransformationDispenseBehavior extends DefaultDispenseItemBehavior 
 	@Override
 	protected ItemStack execute(BlockSource source, ItemStack stack) {
 		Level level = source.level();
-		RandomSource random = level.getRandom();
 		BlockPos blockpos = source.pos().relative(source.state().getValue(DispenserBlock.FACING));
 		if (!level.isClientSide()) {
 			for (LivingEntity livingentity : level.getEntitiesOfClass(LivingEntity.class, new AABB(blockpos), EntitySelector.NO_SPECTATORS)) {
-				level.getRecipeManager().getAllRecipesFor(TFRecipes.TRANSFORM_POWDER_RECIPE.get()).forEach(recipeHolder -> {
-					if (recipeHolder.value().input() == livingentity.getType() || (recipeHolder.value().isReversible() && recipeHolder.value().result() == livingentity.getType())) {
-						EntityType<?> type = recipeHolder.value().isReversible() && recipeHolder.value().result() == livingentity.getType() ? recipeHolder.value().input() : recipeHolder.value().result();
-						Entity newEntity = type.create(level);
-						if (newEntity != null) {
-							newEntity.moveTo(livingentity.getX(), livingentity.getY(), livingentity.getZ(), livingentity.getYRot(), livingentity.getXRot());
-							if (newEntity instanceof Mob mob && livingentity.level() instanceof ServerLevelAccessor accessor) {
-								EventHooks.onFinalizeSpawn(mob, accessor, livingentity.level().getCurrentDifficultyAt(livingentity.blockPosition()), MobSpawnType.CONVERSION, null, null);
-							}
-
-							try {
-								UUID uuid = newEntity.getUUID();
-								newEntity.load(livingentity.saveWithoutId(newEntity.saveWithoutId(new CompoundTag())));
-								newEntity.setUUID(uuid);
-							} catch (Exception e) {
-								TwilightForestMod.LOGGER.warn("Couldn't transform entity NBT data", e);
-							}
-
-							livingentity.level().addFreshEntity(newEntity);
-							livingentity.discard();
-
-							if (livingentity instanceof Mob && livingentity.level().isClientSide()) {
-								((Mob) livingentity).spawnAnim();
-								((Mob) livingentity).spawnAnim();
-							}
-							livingentity.playSound(TFSounds.POWDER_USE.get(), 1.0F + random.nextFloat(), random.nextFloat() * 0.7F + 0.3F);
-							stack.shrink(1);
-							this.fired = true;
-						}
-					}
-				});
+				if (TransformPowderItem.transformEntityIfPossible(level, livingentity, stack)) {
+					this.fired = true;
+				}
 			}
 		}
 		return stack;
