bc5d0e4575529104b1f5f3ad701ee4e82b907b8a
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-01-24 13:57:00 -0700

    fixed a debug logspam issue

--- src/main/java/twilightforest/compat/emi/OrderedSlotWidget.java
+++ src/main/java/twilightforest/compat/emi/OrderedSlotWidget.java
@@ -3,6 +3,7 @@ package twilightforest.compat.emi;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.SlotWidget;
+import org.jetbrains.annotations.Nullable;
 import twilightforest.TwilightForestMod;
 
 import java.util.List;
@@ -11,8 +12,9 @@ public class OrderedSlotWidget extends SlotWidget {
 
 	private long lastGenerate = 0L;
 	private final List<EmiStack> list;
+	@Nullable
 	private EmiIngredient stack = null;
-	private long cycleTime;
+	private final long cycleTime;
 
 	public OrderedSlotWidget(List<EmiStack> stacks, int x, int y, long cycleTime) {
 		super(EmiStack.EMPTY, x, y);
@@ -27,7 +29,6 @@ public class OrderedSlotWidget extends SlotWidget {
 			this.lastGenerate = time;
 			int index = (int) (this.lastGenerate % this.list.size());
 			this.stack = this.list.get(index);
-			TwilightForestMod.LOGGER.debug(this.list.size());
 		}
 
 		return this.stack;
