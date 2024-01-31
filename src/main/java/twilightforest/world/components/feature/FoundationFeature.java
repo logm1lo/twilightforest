5e9cf949154feb89e59af3c2666bcf769ef9281e
drullkus <drullkus@gmail.com>
2024-01-30 22:24:48 -0800

    Make Foundation's basement heights & loot configurable

--- src/main/java/twilightforest/world/components/feature/FoundationFeature.java
+++ src/main/java/twilightforest/world/components/feature/FoundationFeature.java
@@ -2,7 +2,7 @@ package twilightforest.world.components.feature;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.valueproviders.FloatProvider;
@@ -10,12 +10,12 @@ import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
-import twilightforest.loot.TFLootTables;
 import twilightforest.util.FeatureLogic;
 import twilightforest.util.FeatureUtil;
 import twilightforest.world.components.feature.config.RuinedFoundationConfig;
@@ -46,10 +46,10 @@ public class FoundationFeature extends Feature<RuinedFoundationConfig> {
 
 		//TODO: chimney?
 
-		// 50% basement chance!
-		if (rand.nextInt(2) == 0) {
+		int basementDepth = config.basementHeight().sample(rand);
+		if (basementDepth > 0) {
 			BlockPos basementCeilingPos = pos.offset(1, -3, 1);
-			generateBasement(xWidth - 2, zWidth - 2, world, basementCeilingPos, rand, config.placeFloorTest(), config.floor(), config.basementPosts());
+			generateBasement(xWidth - 2, zWidth - 2, basementDepth, world, basementCeilingPos, rand, config.placeFloorTest(), config.floor(), config.basementPosts(), config.lootContainer(), config.lootTable());
 		}
 
 		return true;
@@ -85,8 +85,8 @@ public class FoundationFeature extends Feature<RuinedFoundationConfig> {
 		return rand.nextInt(decayRarity + 1) >= 1 ? main : decay;
 	}
 
-	private static void generateBasement(int xWidth, int zWidth, WorldGenLevel world, BlockPos ceilingPos, RandomSource rand, FloatProvider placeFloorTest, BlockStateProvider floor, BlockStateProvider basementPost) {
-		if (xWidth < 1 || zWidth < 1) return;
+	private static void generateBasement(int xWidth, int zWidth, int depth, WorldGenLevel world, BlockPos ceilingPos, RandomSource rand, FloatProvider placeFloorTest, BlockStateProvider floor, BlockStateProvider basementPost, BlockStateProvider lootContainer, ResourceLocation lootTable) {
+		if (xWidth < 1 || zWidth < 1 || depth < 1) return;
 
 		int chestX = rollChestCoord(xWidth, rand);
 		int chestZ = rollChestCoord(zWidth, rand);
@@ -94,29 +94,33 @@ public class FoundationFeature extends Feature<RuinedFoundationConfig> {
 		// clear basement
 		for (int dX = 0; dX <= xWidth; dX++) {
 			for (int dZ = 0; dZ <= zWidth; dZ++) {
-				world.setBlock(ceilingPos.offset(dX, 0, dZ), Blocks.AIR.defaultBlockState(), 3);
-				world.setBlock(ceilingPos.offset(dX, -1, dZ), Blocks.AIR.defaultBlockState(), 3);
-				world.setBlock(ceilingPos.offset(dX, -2, dZ), Blocks.AIR.defaultBlockState(), 3);
-
 				int cornerOverlap = 0;
 				if (dX == 0) cornerOverlap++;
 				if (dZ == 0) cornerOverlap++;
 				if (dX == xWidth) cornerOverlap++;
 				if (dZ == zWidth) cornerOverlap++;
 
-				if (cornerOverlap > 1) {
-					setAndUpdate(world, rand, basementPost, ceilingPos.offset(dX, 0, dZ));
-					setAndUpdate(world, rand, basementPost, ceilingPos.offset(dX, -1, dZ));
-					setAndUpdate(world, rand, basementPost, ceilingPos.offset(dX, -2, dZ));
-				} else if ((dX == chestX && dZ == chestZ) || (cornerOverlap == 0 && placeFloorTest.sample(rand) <= 0)) {
+				boolean isInCorner = cornerOverlap > 1;
+
+				for (int dY = 1 - depth; dY <= 0; dY++) {
+					BlockPos placeAt = ceilingPos.offset(dX, dY, dZ);
+					world.setBlock(placeAt, Blocks.AIR.defaultBlockState(), 3);
+					if (isInCorner) setAndUpdate(world, rand, basementPost, placeAt);
+				}
+
+				if ((dX == chestX && dZ == chestZ) || (cornerOverlap == 0 && placeFloorTest.sample(rand) <= 0)) {
 					// destroyed wooden plank floor, placed by chance or because a chest is going to generate above it
-					setAndUpdate(world, rand, floor, ceilingPos.offset(dX, -3, dZ));
+					setAndUpdate(world, rand, floor, ceilingPos.offset(dX, -depth, dZ));
 				}
 			}
 		}
 
 		// make chest
-		TFLootTables.FOUNDATION_BASEMENT.generateChest(world, ceilingPos.offset(chestX, -2, chestZ), Direction.NORTH, false);
+		BlockPos lootPos = ceilingPos.offset(chestX, 1 - depth, chestZ);
+		world.setBlock(lootPos, lootContainer.getState(rand, lootPos), 3);
+		if (world.getBlockEntity(lootPos) instanceof RandomizableContainerBlockEntity lootBE) {
+			lootBE.setLootTable(lootTable, world.getSeed() * lootPos.getX() + lootPos.getY() ^ lootPos.getZ());
+		}
 	}
 
 	private static int rollChestCoord(int width, RandomSource rand) {
--- src/main/java/twilightforest/world/components/feature/config/RuinedFoundationConfig.java
+++ src/main/java/twilightforest/world/components/feature/config/RuinedFoundationConfig.java
@@ -5,24 +5,27 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Direction;
 import net.minecraft.data.BlockFamilies;
 import net.minecraft.data.BlockFamily;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.random.SimpleWeightedRandomList;
-import net.minecraft.util.valueproviders.FloatProvider;
-import net.minecraft.util.valueproviders.IntProvider;
-import net.minecraft.util.valueproviders.UniformFloat;
-import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.util.valueproviders.*;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider;
 import twilightforest.data.TFBlockFamilies;
+import twilightforest.loot.TFLootTables;
 
 public record RuinedFoundationConfig(
 		IntProvider wallWidth,
 		IntProvider wallHeights,
+		IntProvider basementHeight,
 		FloatProvider placeFloorTest,
 		BlockStateProvider floor,
 		BlockStateProvider basementPosts,
+		BlockStateProvider lootContainer,
+		ResourceLocation lootTable,
 		// Blockstate given is used for the north-facing wall. Rotations will apply on other-facing walls, corners resolving randomly.
 		BlockStateProvider wallBlock,
 		BlockStateProvider wallTop,
@@ -32,9 +35,12 @@ public record RuinedFoundationConfig(
 	public static final Codec<RuinedFoundationConfig> CODEC = RecordCodecBuilder.create(inst -> inst.group(
 			IntProvider.codec(1, 16).fieldOf("wall_width").forGetter(RuinedFoundationConfig::wallWidth),
 			IntProvider.codec(1, 32).fieldOf("wall_heights").forGetter(RuinedFoundationConfig::wallHeights),
+			IntProvider.codec(0, 16).fieldOf("basement_height").forGetter(RuinedFoundationConfig::basementHeight),
 			FloatProvider.codec(-8, 8).fieldOf("random_floor_chance").forGetter(RuinedFoundationConfig::placeFloorTest),
 			BlockStateProvider.CODEC.fieldOf("floor").forGetter(RuinedFoundationConfig::floor),
 			BlockStateProvider.CODEC.fieldOf("basement_posts").forGetter(RuinedFoundationConfig::basementPosts),
+			BlockStateProvider.CODEC.fieldOf("loot_container").forGetter(RuinedFoundationConfig::lootContainer),
+			ResourceLocation.CODEC.fieldOf("loot_table").forGetter(RuinedFoundationConfig::lootTable),
 			BlockStateProvider.CODEC.fieldOf("wall_block").forGetter(RuinedFoundationConfig::wallBlock),
 			BlockStateProvider.CODEC.fieldOf("wall_top_block").forGetter(RuinedFoundationConfig::wallTop),
 			BlockStateProvider.CODEC.fieldOf("decayed_wall_block").forGetter(RuinedFoundationConfig::decayedWall),
@@ -84,6 +90,8 @@ public record RuinedFoundationConfig(
 						.build()
 				),
 				BlockStateProvider.simple(floorMaterial.get(basementSupports).defaultBlockState()),
+				BlockStateProvider.simple(Blocks.CHEST),
+				TFLootTables.FOUNDATION_BASEMENT.lootTable,
 				BlockStateProvider.simple(wallBlock),
 				new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder()
 						.add(wallBlock, 5)
@@ -103,12 +111,18 @@ public record RuinedFoundationConfig(
 		);
 	}
 
-	public static RuinedFoundationConfig numbersDefault(BlockStateProvider floor, BlockStateProvider basementPosts, BlockStateProvider wallBlock, BlockStateProvider wallTop, BlockStateProvider decayedWall, BlockStateProvider decayedTop) {
+	public static RuinedFoundationConfig numbersDefault(BlockStateProvider floor, BlockStateProvider basementPosts, BlockStateProvider lootBlock, ResourceLocation lootTable, BlockStateProvider wallBlock, BlockStateProvider wallTop, BlockStateProvider decayedWall, BlockStateProvider decayedTop) {
 		return new RuinedFoundationConfig(
 				UniformInt.of(5, 9),
 				UniformInt.of(1, 5),
+				new WeightedListInt(SimpleWeightedRandomList.<IntProvider>builder()
+						// 50% basement chance!
+						.add(ConstantInt.of(3), 1)
+						.add(ConstantInt.of(0), 1)
+						.build()
+				),
 				UniformFloat.of(-2, 1),
-				floor, basementPosts, wallBlock, wallTop, decayedWall, decayedTop
+				floor, basementPosts, lootBlock, lootTable, wallBlock, wallTop, decayedWall, decayedTop
 		);
 	}
 }
