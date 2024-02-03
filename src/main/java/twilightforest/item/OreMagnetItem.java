54e20f127f92fc5836d2c456fcce7972ef42f0b1
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-02-02 19:45:51 -0700

    fixed #1981

--- src/main/java/twilightforest/item/OreMagnetItem.java
+++ src/main/java/twilightforest/item/OreMagnetItem.java
@@ -292,6 +292,12 @@ public class OreMagnetItem extends Item {
 				}
 			}
 		}
+		
+		//Gonna need to special case this one as it isn't covered by tags.
+		//Ancient debris isn't exactly an ore, so it makes sense that the tag doesn't include it
+		if (!ORE_TO_BLOCK_REPLACEMENTS.containsKey(Blocks.ANCIENT_DEBRIS)) {
+			ORE_TO_BLOCK_REPLACEMENTS.put(Blocks.ANCIENT_DEBRIS, Blocks.NETHERRACK);
+		}
 
 		cacheNeedsBuild = false;
 	}
