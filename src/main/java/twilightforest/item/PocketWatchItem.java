4e1950f25d4c2d8ddd7b9b81576e58331e7fd1eb
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-02-04 19:23:28 -0700

    update neo, fix pocket watch effects

--- src/main/java/twilightforest/item/PocketWatchItem.java
+++ src/main/java/twilightforest/item/PocketWatchItem.java
@@ -7,6 +7,7 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -25,8 +26,9 @@ public class PocketWatchItem extends Item {
     @Override
     public void inventoryTick(ItemStack stack, Level level, Entity entity, int slot, boolean held) {
         if (entity instanceof LivingEntity living) {
-            if (slot >= 0 && slot <= 8) {
+            if ((slot >= 0 && slot <= 8) || slot == Inventory.SLOT_OFFHAND) {
                 living.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 5, 0, false, false, false));
+                living.addEffect(new MobEffectInstance(MobEffects.JUMP, 5, 0, false, false, false));
             }
 
             if (living.isHolding(this)) {
