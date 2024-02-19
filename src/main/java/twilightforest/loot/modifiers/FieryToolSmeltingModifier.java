8b30c2da2561b51185528c1bc0bba220a6c96860
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-02-19 12:34:00 -0700

    fiery pickaxe will now drop XP when smelting items

--- src/main/java/twilightforest/loot/modifiers/FieryToolSmeltingModifier.java
+++ src/main/java/twilightforest/loot/modifiers/FieryToolSmeltingModifier.java
@@ -4,14 +4,19 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
 import net.neoforged.neoforge.common.loot.LootModifier;
-import net.neoforged.neoforge.items.ItemHandlerHelper;
+import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FieryToolSmeltingModifier extends LootModifier {
 	public static final Codec<FieryToolSmeltingModifier> CODEC = RecordCodecBuilder.create(inst -> LootModifier.codecStart(inst).apply(inst, FieryToolSmeltingModifier::new));
@@ -22,14 +27,18 @@ public class FieryToolSmeltingModifier extends LootModifier {
 
 	@Override
 	protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-		ObjectArrayList<ItemStack> newLoot = new ObjectArrayList<>();
-		generatedLoot.forEach((stack) -> newLoot.add(
-				context.getLevel().getRecipeManager()
-						.getRecipeFor(RecipeType.SMELTING, new SimpleContainer(stack), context.getLevel())
-						.map(holder -> holder.value().getResultItem(context.getLevel().registryAccess()))
-						.filter(itemStack -> !itemStack.isEmpty())
-						.map(itemStack -> ItemHandlerHelper.copyStackWithSize(itemStack, stack.getCount() * itemStack.getCount()))
-						.orElse(stack)));
+		List<Pair<ItemStack, Float>> list = generatedLoot.stream().map(stack ->
+			context.getLevel().getRecipeManager().getRecipeFor(RecipeType.SMELTING, new SimpleContainer(stack), context.getLevel())
+					.map(holder -> Pair.of(holder.value().getResultItem(context.getLevel().registryAccess()), holder.value().getExperience()))
+					.orElse(Pair.of(stack, 0.0F))).toList();
+
+		ObjectArrayList<ItemStack> newLoot = list.stream().map(Pair::getLeft).collect(Collectors.toCollection(ObjectArrayList::new));
+		float xp = (float) list.stream().mapToDouble(Pair::getRight).sum();
+
+		if (xp > 0.0F && context.hasParam(LootContextParams.THIS_ENTITY)) {
+			ExperienceOrb.award(context.getLevel(), context.getParam(LootContextParams.THIS_ENTITY).position(), Math.round(xp));
+		}
+
 		return newLoot;
 	}
 
