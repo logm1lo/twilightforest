6e458e64d7f5968b99b3b3897d371cf1fd55194d
drullkus <drullkus@gmail.com>
2024-04-27 18:06:21 -0700

    Fix banners

--- src/main/java/twilightforest/init/TFBannerPatterns.java
+++ src/main/java/twilightforest/init/TFBannerPatterns.java
@@ -1,6 +1,5 @@
 package twilightforest.init;
 
-import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.entity.BannerPattern;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -10,14 +9,17 @@ import twilightforest.TwilightForestMod;
 public class TFBannerPatterns {
 	public static final DeferredRegister<BannerPattern> BANNER_PATTERNS = DeferredRegister.create(Registries.BANNER_PATTERN, TwilightForestMod.ID);
 
-	public static final DeferredHolder<BannerPattern, BannerPattern> NAGA = BANNER_PATTERNS.register("naga", () -> new BannerPattern("tfn"));
-	public static final DeferredHolder<BannerPattern, BannerPattern> LICH = BANNER_PATTERNS.register("lich", () -> new BannerPattern("tfl"));
-	public static final DeferredHolder<BannerPattern, BannerPattern> MINOSHROOM = BANNER_PATTERNS.register("minoshroom", () -> new BannerPattern("tfm"));
-	public static final DeferredHolder<BannerPattern, BannerPattern> HYDRA = BANNER_PATTERNS.register("hydra", () -> new BannerPattern("tfh"));
-	public static final DeferredHolder<BannerPattern, BannerPattern> KNIGHT_PHANTOM = BANNER_PATTERNS.register("knight_phantom", () -> new BannerPattern("tfp"));
-	public static final DeferredHolder<BannerPattern, BannerPattern> UR_GHAST = BANNER_PATTERNS.register("ur_ghast", () -> new BannerPattern("tfg"));
-	public static final DeferredHolder<BannerPattern, BannerPattern> ALPHA_YETI = BANNER_PATTERNS.register("alpha_yeti", () -> new BannerPattern("tfy"));
-	public static final DeferredHolder<BannerPattern, BannerPattern> SNOW_QUEEN = BANNER_PATTERNS.register("snow_queen", () -> new BannerPattern("tfq"));
-	public static final DeferredHolder<BannerPattern, BannerPattern> QUEST_RAM = BANNER_PATTERNS.register("quest_ram", () -> new BannerPattern("tfr"));
+	public static final DeferredHolder<BannerPattern, BannerPattern> NAGA = register("naga", "tfn");
+	public static final DeferredHolder<BannerPattern, BannerPattern> LICH = register("lich", "tfl");
+	public static final DeferredHolder<BannerPattern, BannerPattern> MINOSHROOM = register("minoshroom", "tfm");
+	public static final DeferredHolder<BannerPattern, BannerPattern> HYDRA = register("hydra", "tfh");
+	public static final DeferredHolder<BannerPattern, BannerPattern> KNIGHT_PHANTOM = register("knight_phantom", "tfp");
+	public static final DeferredHolder<BannerPattern, BannerPattern> UR_GHAST = register("ur_ghast", "tfg");
+	public static final DeferredHolder<BannerPattern, BannerPattern> ALPHA_YETI = register("alpha_yeti", "tfy");
+	public static final DeferredHolder<BannerPattern, BannerPattern> SNOW_QUEEN = register("snow_queen", "tfq");
+	public static final DeferredHolder<BannerPattern, BannerPattern> QUEST_RAM = register("quest_ram", "tfr");
 
+	private static DeferredHolder<BannerPattern, BannerPattern> register(String name, String translationKey) {
+		return BANNER_PATTERNS.register(name, () -> new BannerPattern(TwilightForestMod.prefix(name), translationKey));
+	}
 }
