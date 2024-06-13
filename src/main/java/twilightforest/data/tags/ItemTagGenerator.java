7698becd8501ed020aa3d25b8accee7a3983396f
jodi / jodlodi <jodlodi090@gmail.com>
2024-06-13 03:35:55 +0200

    Added chiseled canopy book shelf to NYI tag

--- src/main/java/twilightforest/data/tags/ItemTagGenerator.java
+++ src/main/java/twilightforest/data/tags/ItemTagGenerator.java
@@ -218,7 +218,8 @@ public class ItemTagGenerator extends ModdedItemTagGenerator {
 			TFBlocks.SLIDER.get().asItem(),
 			TFItems.MAGIC_PAINTING.get(),
 			TFItems.QUEST_RAM_BANNER_PATTERN.get(),
-			TFBlocks.FINAL_BOSS_BOSS_SPAWNER.asItem()
+			TFBlocks.FINAL_BOSS_BOSS_SPAWNER.asItem(),
+			TFBlocks.CHISELED_CANOPY_BOOKSHELF.asItem()
 		);
 
 		this.tag(KOBOLD_PACIFICATION_BREADS).add(Items.BREAD);
