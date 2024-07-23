72507088f8af17aaed273a5dc7e1ac74a3c0e67d
Tamaized <9671313+Tamaized@users.noreply.github.com>
2024-07-22 21:24:30 -0400

    Fix flask instant use and cleanup bad Optional usage

--- src/main/java/twilightforest/components/entity/PotionFlaskTrackingAttachment.java
+++ src/main/java/twilightforest/components/entity/PotionFlaskTrackingAttachment.java
@@ -7,47 +7,46 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.alchemy.Potion;
-import net.minecraft.world.item.alchemy.Potions;
 import org.jetbrains.annotations.Nullable;
 import twilightforest.init.TFAdvancements;
 
-import java.util.Optional;
+import java.util.Objects;
 
 public class PotionFlaskTrackingAttachment {
 
-	private Optional<Holder<Potion>> lastUsedPotion;
-	private int doses;
+	@Nullable
+	private Holder<Potion> lastUsedPotion;
+	private int dosesDrank;
 	private long lastTimeStarted;
 
 	public static final Codec<PotionFlaskTrackingAttachment> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-			BuiltInRegistries.POTION.holderByNameCodec().optionalFieldOf("last_used_potion").forGetter(o -> o.lastUsedPotion),
-			Codec.INT.fieldOf("doses_drank").forGetter(o -> o.doses),
+			BuiltInRegistries.POTION.holderByNameCodec().optionalFieldOf("last_used_potion", null).forGetter(o -> o.lastUsedPotion),
+			Codec.INT.fieldOf("doses_drank").forGetter(o -> o.dosesDrank),
 			Codec.LONG.fieldOf("last_game_time_started").forGetter(o -> o.lastTimeStarted))
 		.apply(instance, PotionFlaskTrackingAttachment::new));
 
 	public PotionFlaskTrackingAttachment() {
-		this(Optional.empty(), 0, 0);
+		this(null, 0, 0);
 	}
 
-	public PotionFlaskTrackingAttachment(Optional<Holder<Potion>> lastUsedPotion, int doses, long timeStarted) {
+	public PotionFlaskTrackingAttachment(@Nullable Holder<Potion> lastUsedPotion, int dosesDrank, long timeStarted) {
 		this.lastUsedPotion = lastUsedPotion;
-		this.doses = doses;
+		this.dosesDrank = dosesDrank;
 		this.lastTimeStarted = timeStarted;
 	}
 
-	public void incrementDoses(Optional<Holder<Potion>> potion, ServerPlayer player) {
-		if (potion.isEmpty()) return;
+	public void trackDrink(Holder<Potion> potion, ServerPlayer player) {
 		//reset advancement window if potion changed. Otherwise, continue incrementing the doses
-		if (this.lastUsedPotion.isEmpty() || this.lastUsedPotion != potion) {
-			this.doses = 1;
+		if (this.lastUsedPotion == null || !Objects.equals(this.lastUsedPotion, potion)) {
+			this.dosesDrank = 1;
 			this.lastTimeStarted = player.level().getGameTime();
 		} else {
-			this.doses++;
+			this.dosesDrank++;
 		}
 		this.lastUsedPotion = potion;
 
 		if (player.isAlive()) {
-			TFAdvancements.DRINK_FROM_FLASK.get().trigger(player, this.doses, Mth.floor((float) (player.level().getGameTime() - this.lastTimeStarted) / 20L), this.lastUsedPotion);
+			TFAdvancements.DRINK_FROM_FLASK.get().trigger(player, this.dosesDrank, Mth.floor((float) (player.level().getGameTime() - this.lastTimeStarted) / 20L), this.lastUsedPotion);
 		}
 	}
 }
