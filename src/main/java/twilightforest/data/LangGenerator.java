16c19a8cd7cb4b70b5c54ff88504d1e49ec87537
AriaElidove <senthebun@gmail.com>
2024-08-13 00:37:38 +0200

    Tranformation -> Transformation

--- src/main/java/twilightforest/data/LangGenerator.java
+++ src/main/java/twilightforest/data/LangGenerator.java
@@ -1070,7 +1070,7 @@ public class LangGenerator extends TFLangProvider {
 
 		this.configCategory("magic_trees", "Magic Trees", ConfigComments.MAGIC_TREES);
 		this.configEntry("time_range", "Tree of Time Range", ConfigComments.TIME_CORE);
-		this.configEntry("transformation_range", "Tree of Transformation Range", ConfigComments.TRANFORMATION_CORE);
+		this.configEntry("transformation_range", "Tree of Transformation Range", ConfigComments.TRANSFORMATION_CORE);
 		this.configEntry("mining_range", "Miner's Tree Range", ConfigComments.MINING_CORE);
 		this.configEntry("sorting_range", "Sorting Tree Range", ConfigComments.SORTING_CORE);
 
