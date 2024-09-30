5d809ecb3e8c578392d62d64354a9c1f3ef13037
drullkus <drullkus@gmail.com>
2024-09-30 00:47:39 -0700

    forgot this

--- src/main/java/twilightforest/world/components/structures/lichtowerrevamp/LichTowerWingBridge.java
+++ src/main/java/twilightforest/world/components/structures/lichtowerrevamp/LichTowerWingBridge.java
@@ -1,10 +1,15 @@
 package twilightforest.world.components.structures.lichtowerrevamp;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.FrontAndTop;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.levelgen.structure.StructurePiece;
 import net.minecraft.world.level.levelgen.structure.StructurePieceAccessor;
@@ -13,6 +18,7 @@ import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceSeriali
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 import net.neoforged.neoforge.common.world.PieceBeardifierModifier;
 import org.jetbrains.annotations.Nullable;
+import twilightforest.data.tags.BlockTagGenerator;
 import twilightforest.init.TFStructurePieceTypes;
 import twilightforest.util.jigsaw.JigsawPlaceContext;
 import twilightforest.util.jigsaw.JigsawRecord;
@@ -20,20 +26,49 @@ import twilightforest.world.components.structures.TwilightJigsawPiece;
 import twilightforest.world.components.structures.util.SortablePiece;
 
 public final class LichTowerWingBridge extends TwilightJigsawPiece implements PieceBeardifierModifier, SortablePiece {
+	private final boolean fromCentral;
+
 	public LichTowerWingBridge(StructurePieceSerializationContext ctx, CompoundTag compoundTag) {
 		super(TFStructurePieceTypes.LICH_WING_BRIDGE.get(), compoundTag, ctx, readSettings(compoundTag));
 
 		LichTowerUtil.addDefaultProcessors(this.placeSettings);
+		this.fromCentral = compoundTag.getBoolean("from_central");
 	}
 
-	public LichTowerWingBridge(StructureTemplateManager structureManager, int genDepth, JigsawPlaceContext jigsawContext, ResourceLocation templateLocation) {
+	public LichTowerWingBridge(StructureTemplateManager structureManager, int genDepth, JigsawPlaceContext jigsawContext, ResourceLocation templateLocation, boolean fromCentral) {
 		super(TFStructurePieceTypes.LICH_WING_BRIDGE.get(), genDepth, structureManager, templateLocation, jigsawContext);
 
 		LichTowerUtil.addDefaultProcessors(this.placeSettings);
+		this.fromCentral = fromCentral;
 	}
 
 	@Override
 	protected void processJigsaw(StructurePiece parent, StructurePieceAccessor pieceAccessor, RandomSource random, JigsawRecord record, int jigsawIndex) {
+	}
+
+	@Override
+	public void postProcess(WorldGenLevel level, StructureManager structureManager, ChunkGenerator chunkGen, RandomSource random, BoundingBox chunkBounds, ChunkPos chunkPos, BlockPos structureCenterPos) {
+		super.postProcess(level, structureManager, chunkGen, random, chunkBounds, chunkPos, structureCenterPos);
+
+		if (this.fromCentral) {
+			JigsawRecord sourceJigsaw = this.getSourceJigsaw();
+			BlockPos sourcePos = this.templatePosition.offset(sourceJigsaw.pos());
+			BlockPos leftPos = sourcePos.relative(sourceJigsaw.orientation().front().getClockWise(Direction.Axis.Y));
+			BlockPos rightPos = sourcePos.relative(sourceJigsaw.orientation().front().getCounterClockWise(Direction.Axis.Y));
+
+			removeIfBanister(level, leftPos, chunkBounds);
+			removeIfBanister(level, leftPos.above(), chunkBounds);
+			removeIfBanister(level, rightPos, chunkBounds);
+			removeIfBanister(level, rightPos.below(), chunkBounds);
+		}
+	}
+
+	private static void removeIfBanister(WorldGenLevel level, BlockPos pos, BoundingBox chunkBounds) {
+		if (chunkBounds.isInside(pos)) {
+			if (level.getBlockState(pos).is(BlockTagGenerator.BANISTERS)) {
+				level.removeBlock(pos, false);
+			}
+		}
 	}
 
 	@Override
@@ -80,7 +115,7 @@ public final class LichTowerWingBridge extends TwilightJigsawPiece implements Pi
 		JigsawPlaceContext placeableJunction = JigsawPlaceContext.pickPlaceableJunction(parent.templatePosition(), sourceJigsawPos, sourceOrientation, structureManager, bridgeId, fromCentralTower ? "twilightforest:lich_tower/bridge_center" : "twilightforest:lich_tower/bridge", random);
 
 		if (placeableJunction != null) {
-			LichTowerWingBridge bridge = new LichTowerWingBridge(structureManager, newDepth, placeableJunction, bridgeId);
+			LichTowerWingBridge bridge = new LichTowerWingBridge(structureManager, newDepth, placeableJunction, bridgeId, fromCentralTower);
 
 			if ((allowClipping || pieceAccessor.findCollisionPiece(bridge.boundingBox) == null) && bridge.tryGenerateRoom(random, pieceAccessor, roomMaxSize, generateGround, fromCentralTower)) {
 				// If the bridge & room can be fitted, then also add bridge to list then exit this function
@@ -98,7 +133,7 @@ public final class LichTowerWingBridge extends TwilightJigsawPiece implements Pi
 		JigsawPlaceContext placeableJunction = JigsawPlaceContext.pickPlaceableJunction(parent.templatePosition(), sourceJigsawPos, sourceOrientation, structureManager, bridgeCoverLocation, "twilightforest:lich_tower/bridge", random);
 
 		if (placeableJunction != null) {
-			StructurePiece bridgeCoverPiece = new LichTowerWingBridge(structureManager, newDepth, placeableJunction, bridgeCoverLocation);
+			StructurePiece bridgeCoverPiece = new LichTowerWingBridge(structureManager, newDepth, placeableJunction, bridgeCoverLocation, false);
 			pieceAccessor.addPiece(bridgeCoverPiece);
 			bridgeCoverPiece.addChildren(parent, pieceAccessor, random);
 		}
