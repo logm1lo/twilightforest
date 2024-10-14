984b989f8aee9e1b32418d04b5c0c93a4ee271f4
jodi / jodlodi <jodlodi090@gmail.com>
2024-10-14 05:41:50 +0200

    i may be stupid

--- src/main/java/twilightforest/util/entities/EntityExcludedDamageSource.java
+++ src/main/java/twilightforest/util/entities/EntityExcludedDamageSource.java
@@ -4,8 +4,10 @@ import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,6 +15,11 @@ import java.util.List;
 public class EntityExcludedDamageSource extends DamageSource {
 
 	protected final List<EntityType<?>> entities;
+
+	public EntityExcludedDamageSource(Holder<DamageType> type, @Nullable Entity directEntity, @Nullable Entity causingEntity, EntityType<?>... entities) {
+		super(type, directEntity, causingEntity);
+		this.entities = Arrays.stream(entities).toList();
+	}
 
 	public EntityExcludedDamageSource(Holder<DamageType> type, EntityType<?>... entities) {
 		super(type);
